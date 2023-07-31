17ec9a964875fa51f729afe517e429f22525f264
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-07-31 20:34:51 -0300

    Add files via upload

---
+++ projMVC/projMVC/Coisas.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projMVC
+{
+    internal class Coisas
+    {
+        private Coisa[] asCoisas;
+        private int max;
+        private int qtde;
+
+        public int Max { 
+            get => max; 
+        }
+        public int Qtde {
+            get => qtde; 
+        }
+        internal Coisa[] AsCoisas {
+            get => asCoisas; 
+        }
+
+        public Coisas(int max)
+        {
+            this.max = max;
+            this.qtde = 0;
+            this.asCoisas = new Coisa[this.max];
+            for(int i= 0; i < this.max; i++)
+            {
+                this.asCoisas[i] = new Coisa(-1, "...");
+            }
+        }
+
+        public string mostrar()
+        {
+            string s = "";
+            foreach(Coisa c in this.asCoisas)
+            {
+                if (c.Id != -1)
+                {
+                    s += c.ToString();
+                }
+            }
+            return s;
+        }
+
+        public bool adicionar(Coisa c)
+        {
+            bool podeAdicionar = (this.qtde < this.max);
+            if (podeAdicionar)
+            {
+                this.asCoisas[this.qtde++] = c;
+            }
+            return podeAdicionar;
+        }
+
+        public Coisa pesquisar(Coisa c)
+        {
+            Coisa coisaAchada = new Coisa(-1, "...");
+            /*
+            int i = 0;
+            while (i < this.max && !this.asCoisas[i].Equals(c))
+            {
+                i++;
+            }
+            if (i<this.max)
+            {
+                coisaAchada = this.asCoisas[i];
+            }
+            */
+            foreach(Coisa co in this.asCoisas)
+            {
+                if (co.Equals(c))
+                {
+                    coisaAchada = co;
+                    break;
+                }
+            }
+            return coisaAchada;
+        }
+
+        public bool remover(Coisa c)
+        {
+            bool podeRemover;
+            
+            int i = 0;
+            while (i < this.max && !this.asCoisas[i].Equals(c))
+            {
+                i++;
+            }
+            podeRemover = (i < this.max);
+           
+            if (podeRemover)
+            {
+                while (i < this.max-1)
+                {
+                    this.asCoisas[i] = this.asCoisas[i+1];
+                    i++;
+                }
+                this.asCoisas[i] = new Coisa(-1, "...");
+                this.qtde--;
+            }
+            return podeRemover;
+        }
+    }
+
+
+}
---
+++ projMVC/projMVC/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projMVC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coisas minhasCoisas = new Coisas(5);
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(1, "Coisa 1"))) ? "Adicionou":"Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(2, "Coisa 2"))) ? "Adicionou" : "Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(3, "Coisa 3"))) ? "Adicionou" : "Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(4, "Coisa 4"))) ? "Adicionou" : "Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(5, "Coisa 5"))) ? "Adicionou" : "Não adicionou");
+            Console.WriteLine((minhasCoisas.adicionar(new Coisa(6, "Coisa 6"))) ? "Adicionou" : "Não adicionou");
+
+            Console.WriteLine(minhasCoisas.mostrar());
+
+            Coisa coisaPesquisada = minhasCoisas.pesquisar(new Coisa(3, "Coisa 3"));
+            if (coisaPesquisada.Id != -1)
+            {
+                Console.WriteLine("Coisa achada: " + coisaPesquisada.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Coisa não encontrada");
+            }
+
+            Console.WriteLine(minhasCoisas.pesquisar(new Coisa(2)).ToString());
+
+            Console.WriteLine(minhasCoisas.remover(new Coisa(3)) ? "Removeu" : "Não removeu");
+            Console.WriteLine(minhasCoisas.remover(new Coisa(3)) ? "Removeu" : "Não removeu");
+
+
+
+
+        }
+    }
+}
